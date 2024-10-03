cfd3640305ed66dc451e6b366622da47eed02ae9
Maria Victória Ribeiro <mvictoria-ribeiro@hotmail.com>
2024-10-02 21:57:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var numero1 = 2024
+var numero2 = 1995
+
+console.log(2024 - 1995);
+
+// cls - limpar a tela  #Windows 
+// clear - limpar a tela #MacOS/ Linux
+// node index.js
+
+//console. log () '.' sig acessar função
+
+var // variaveis  que sao caria o valor
+const// variaveis constantes, que o valor nunca mudará
+let// variáveis que podem mudar e serão descartadas
+
+// Nunca uniciar o nome com um número - var 1nome; (errado)
+// Nunca acentuar a variavel - var profissão (errado)
+// O único caracter especial aceito é o underline/underscon _ / var _nome / var nome_profissao;
+
+// snake_case: Seprados por underline
+// PascalCase: Palavras iniciadas em letras MAIÚSCULAS
+// camelCase : palavras que se são separadas por letras MAISC
+
+
+// frase2= ´ O nome da pessoa é ${nome}. E sua idade é ${idade} anos´; INTERPOLADOR
